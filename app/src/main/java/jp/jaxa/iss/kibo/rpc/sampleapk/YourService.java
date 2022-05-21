77391d2dd7e7d99989ac889c517d57960222aaf3
jasonlin1222 <jasonlin1222@gmail.com>
2022-05-21 22:15:12 +0800

    add log file and enable laser at point 2

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -23,23 +23,19 @@ public class YourService extends KiboRpcService {
 
         // move to point 1
         Point point = new Point(10.71f, -7.7f, 4.48f);
-        Quaternion quaternion = new Quaternion(0f, 0.707f, 0f, 0.707f);
+        Quaternion quaternion = new Quaternion(0f, 0.707f, 0f, 0.707f); // TODO: adjust angle
         api.moveTo(point, quaternion, true);
 
         // report point1 arrival
         api.reportPoint1Arrival();
 
-        // get a camera image
-        Mat image = api.getMatNavCam();
-
-        //target detection: TODO
+        // first target detection: TODO
 
         // irradiate the laser
         api.laserControl(true);
 
-        //debug cam
+        // first debug cam
         Mat debug_point_1 = api.getMatNavCam();
-
         api.saveMatImage(debug_point_1, "point1");
 
         // take target1 snapshots
@@ -48,21 +44,38 @@ public class YourService extends KiboRpcService {
         // turn the laser off
         api.laserControl(false);
 
-        /* ******************************************** */
-        /* write your own code and repair the air leak! */
-        /* ******************************************** */
-        Log.d("start", "start of moving to point 2");
+        // debug console
+        Log.d("game", "Point 1 complete");
+
         //move to point 2
         Point point2 = new Point(11.2746f, -9.92284f,  5.29881f);
         Point avoid = new Point(11.3f, -8.2f, 4.48f);
         Point avoid2 = new Point(11.3f, -9.5f, 4.48f);
         Quaternion quaternion2 = new Quaternion(0f, 0f, -0.707f, 0.707f);
         api.moveTo(avoid, quaternion2, true);
-        Log.d("pos", "move to avoid");
+        Log.d("movement", "move to avoid");
         api.moveTo(avoid2, quaternion2, true);
-        Log.d("pos", "move to avoid2");
+        Log.d("movement", "move to avoid 2");
         api.moveTo(point2, quaternion2, true);
-        Log.d("pos", "move to point 2");
+        Log.d("movement", "move to point 2");
+
+        // second target detection: TODO
+
+        // irradiate the laser
+        api.laserControl(true);
+
+        // second debug cam
+        Mat debug_point_2 = api.getMatNavCam();
+        api.saveMatImage(debug_point_2, "point2");
+
+        // take target2 snapshots
+        api.takeTarget2Snapshot();
+
+        // turn the laser off
+        api.laserControl(false);
+
+        // debug console
+        Log.d("game", "Point 2 complete");
 
         // send mission completion
         api.reportMissionCompletion();
